16696fbc419dcb272d84da095702345e09908ff2
emerson espinoza aguirre <emerson@emersons-MacBook-Air.local>
2021-08-29 00:08:02 -0500

    modificando las vistas

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Ya estoy en React </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://emersonespinoza.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Emerson 
         </a>
       </header>
     </div>
